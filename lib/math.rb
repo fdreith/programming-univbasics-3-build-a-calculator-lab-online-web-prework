fa9f00c8a617270b815127570ba8016ae5bef20c
Francesca Dreith <francesca.dreith@gmail.com>
2019-10-23 02:21:25 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,34 +1,23 @@
 def addition(num1, num2)
-  num1= "5"
-  num2= "4"
   puts num2+num1
 end
 
 def subtraction(num1, num2)
-  num2 = "10"
-  num1 = "5"
   puts num2 - num1
 end
 
 def division(num1, num2)
-  num1="2"
-  num2="50"
   puts num2/num1
 end
 
 def multiplication(num1, num2)
-  num1="4"
-  num2="30"
   puts num1*num2
 end
 
 def modulo(num1, num2)
-  num1="5"
-  num2="34"
   puts num2/num1
 end
 
 def square_root(num)
-  num = "81"
   puts math.sqrt(num)
 end
