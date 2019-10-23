6aa258f70fcae6a5e49707592548b1c7e3c50a41
Francesca Dreith <francesca.dreith@gmail.com>
2019-10-23 02:18:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,24 +1,24 @@
 def addition(num1, num2)
-  num1= 5
-  num2= 4
+  num1= "5"
+  num2= "4"
   puts num2+num1
 end
 
 def subtraction(num1, num2)
-  num2 = 10
-  num1 = 5
+  num2 = "10"
+  num1 = "5"
   puts num2 - num1
 end
 
 def division(num1, num2)
-  num1=2
-  num2=50
+  num1="2"
+  num2="50"
   puts num2/num1
 end
 
 def multiplication(num1, num2)
-  num1=4
-  num2=30
+  num1="4"
+  num2="30
   puts num1*num2
 end
 
