f1b59ff40570b83b69133f9ed771f372e2982e69
Francesca Dreith <francesca.dreith@gmail.com>
2019-10-23 02:51:57 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -12,13 +12,13 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
-  puts num1*num2
+  num1*num2
 end
 
 def modulo(num1, num2)
-  puts num2/num1
+  num2/num1
 end
 
 def square_root(num)
-  puts math.sqrt(num)
+  math.sqrt(num)
 end
